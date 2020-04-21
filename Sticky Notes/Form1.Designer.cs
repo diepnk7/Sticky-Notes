6ec95aedf8656b4d77582fc90c7bcff7fce50ca0
Nguyễn Ngọc Diệp <yoyaldev@gmail.com>
2020-04-21 14:30:35 +0700

    Upload all sourcecode

---
+++ Sticky Notes/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Sticky_Notes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnX = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnX
+            // 
+            resources.ApplyResources(this.btnX, "btnX");
+            this.btnX.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnX.ForeColor = System.Drawing.Color.Black;
+            this.btnX.Name = "btnX";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnX;
+    }
+}
+
---
+++ Sticky Notes/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sticky_Notes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //move windows without title bar
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            bool flag = m.Msg == 132;
+            if (flag)
+            {
+                m.Result = (IntPtr)2;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Notes\Notes.txt";
+
+            if (File.Exists(filePath))
+            {
+                //Decrypt
+                textBox1.Text = File.ReadAllText(filePath);
+                
+            }
+            else
+            {
+                File.Create(filePath);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            //Encrypt
+            File.WriteAllText(@"C:\Notes\Notes.txt", textBox1.Text);
+
+            Application.Exit();
+        } 
+    }
+}
